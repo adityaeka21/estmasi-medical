40b7a106c88b87d4e21fc3f13874290f58b0576b
adityaeka21 <148531157+adityaeka21@users.noreply.github.com>
2023-10-20 16:39:04 +0700

    Update estimasi_medical.py

--- estimasi_medical.py
+++ estimasi_medical.py
@@ -10,11 +10,17 @@ age = st.number_input('Umur Pasien')
 sex2 = st.selectbox('Jenis Kelamin', ['Laki-Laki', 'Perempuan'])
 children = st.number_input('Jumlah Tanggungan Anak')
 smoker2 = st.selectbox('Apakah Pasien Merokok', ['Yes', 'No'])
-region2 = st.selectbox('Wilayah Domisili', ['southeast','southwest', 'northwest','northeast'])
+region2 = st.selectbox('Wilayah Domisili', ['southeast', 'southwest', 'northwest', 'northeast'])
 
+# Define LabelEncoders for categorical variables
 label_encoder_sex = LabelEncoder()
 label_encoder_smoker = LabelEncoder()
 label_encoder_region = LabelEncoder()
+
+# Anda perlu melakukan fit pada objek LabelEncoder sebelum melakukan transform
+label_encoder_sex.fit(['Laki-Laki', 'Perempuan'])
+label_encoder_smoker.fit(['Yes', 'No'])
+label_encoder_region.fit(['southeast', 'southwest', 'northwest', 'northeast'])
 
 sex_encoded = label_encoder_sex.transform([sex2])[0]
 smoker_encoded = label_encoder_smoker.transform([smoker2])[0]
@@ -24,7 +30,7 @@ predict = ''
 
 if st.button('Estimasi Harga'):
     predict = model.predict(
-        [[age,sex2,bmi,children,smoker2,region2]]
+        [[age, sex_encoded, children, smoker_encoded, region_encoded]]
     )
-    st.write ('Estimasi Biaya Medis dalam US Dolar : ', predict)
-    st.write ('Estimasi Biaya Medis dalam IDR (Juta) :', predict*15000)
+    st.write('Estimasi Biaya Medis dalam US Dolar:', predict)
+    st.write('Estimasi Biaya Medis dalam IDR (Juta):', predict * 15000)
