eaaec6d1d4bae4430572a99f22442cf3076c2423
adityaeka21 <148531157+adityaeka21@users.noreply.github.com>
2023-10-20 15:39:45 +0700

    Create estimasi_medical.py

---
+++ estimasi_medical.py
@@ -0,0 +1,25 @@
+import pickle
+import streamlit as st
+
+model = pickle.load(open('estimasi_medical.sav', 'rb'))
+
+st.title('Estimasi Asuransi Kesehatan')
+
+age = st.number_input('Umur Pasien')
+sex2 = st.selectbox('Jenis Kelamin', ['Laki-Laki', 'Perempuan'])
+children = st.number_input('Jumlah Tanggungan Anak')
+smoker2 = st.selectbox('Apakah Pasien Merokok', ['Yes', 'No'])
+region2 = st.selectbox('Wilayah Domisili', ['southeast','southwest', 'northwest,'northeast'])
+
+sex_encoded = label_encoder_sex.transform([sex])[0]
+smoker_encoded = label_encoder_smoker.transform([smoker])[0]
+region_encoded = label_encoder_region.transform([region])[0]
+
+predict = ''
+
+if st.button('Estimasi Harga'):
+    predict = model.predict(
+        [[age,sex2,bmi,children,smoker2,region2]]
+    )
+    st.write ('Estimasi Biaya Medis dalam US Dolar : ', predict)
+    st.write ('Estimasi Biaya Medis dalam IDR (Juta) :', predict*15000)
